2d31719bcc735965ba292e2793176cdb99de4650
Mallchad <ncaprisunfan@gmail.com>
2021-03-25 09:24:30 +0000

    Housekeeping: Readjusted some indentation and formatting

--- rc.lua
+++ rc.lua
@@ -478,18 +478,18 @@ root.keys(globalkeys)
 awful.rules.rules = {
    -- All clients will match this rule.
    { rule = { },
-     properties = { border_width = beautiful.border_width,
-                    border_color = beautiful.border_normal,
-                    focus = awful.client.focus.filter,
-                    raise = true,
-                    keys = clientkeys,
-                    buttons = clientbuttons,
-                    screen = awful.screen.preferred,
-                    placement = awful.placement.no_offscreen,
-                    size_hints_honor = false,
+     properties = {
+        border_width = beautiful.border_width,
+        border_color  = beautiful.border_normal,
+        focus = awful.client.focus.filter,
+        raise = true,
+        keys = clientkeys,
+        buttons = clientbuttons,
+        screen = awful.screen.preferred,
+        placement = awful.placement.no_offscreen,
+        size_hints_honor = false,
      }
    },
-
    -- Floating clients.
    { rule_any = {
         instance = {
@@ -544,7 +544,8 @@ awful.rules.rules = {
       properties =
          {  maximized_horizontal = true,
             opacity = 0.95}
-   },-- Set Firefox to always map on the tag named "2" on screen 1.
+   },
+   -- Set Firefox to always map on the tag named "2" on screen 1.
    -- { rule = { class = "Firefox" },
    --   properties = { screen = 1, tag = "2" } },
 }
