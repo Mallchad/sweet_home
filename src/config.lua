2e5dc5359482572e78c0a214cc5f4f51477d39fe
Mallchad <ncaprisunfan@gmail.com>
2023-11-10 13:05:32 +0000

    Tweak: Restored Windows+Tab window switching functionaltiy
    It stays on the current screen and tag for sanity reasons

--- src/config.lua
+++ src/config.lua
@@ -158,36 +158,32 @@ local globalkeys = gears.table.join(
    awful.key({ dat.modkey,           }, "Escape", awful.tag.history.restore,
       {description = "go back", group = "tag"}),
    awful.key({ dat.modkey,           }, "j",
-      function ()
-         awful.client.focus.byidx( 1)
-      end,
+      function () awful.client.focus.byidx( 1) end,
       {description = "focus next by index", group = "client"}
    ),
    awful.key({ dat.modkey,           }, "k",
-      function ()
-         awful.client.focus.byidx(-1)
-      end,
-      {description = "focus previous by index", group = "client"}
+      function () awful.client.focus.byidx(-1)
+      end, {description = "focus previous by index", group = "client"}
+   ),
+   awful.key({ dat.modkey,           }, "Tab",
+      function () awful.client.focus.byidx( 1) end,
+      {description = "focus next by index", group = "client"}
+   ),
+   awful.key({ dat.modkey, "Shift"    }, "Tab",
+      function () awful.client.focus.byidx(-1)
+      end, {description = "focus previous by index", group = "client"}
    ),
    -- Layout manipulation
    awful.key({ dat.modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
-      {description = "swap with next client by index", group = "client"}),
+      {description = "focus next client on screen", group = "client"}),
    awful.key({ dat.modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
-      {description = "swap with previous client by index", group = "client"}),
+      {description = "", group = "focus previous client on screen"}),
    awful.key({ dat.modkey }, "a", focus_primary_screen,
       {description = "focus the primary screen", group = "screen"}),
    awful.key({ dat.modkey }, "d", focus_cycle_screen,
       {description = "cycle focus through non-priamry screens", group = "screen"}),
    awful.key({ dat.modkey,           }, "u", awful.client.urgent.jumpto,
       {description = "jump to urgent client", group = "client"}),
-   awful.key({ dat.modkey,           }, "Tab",
-      function ()
-         awful.client.focus.history.previous()
-         if client.focus then
-            client.focus:raise()
-         end
-      end,
-      {description = "go back", group = "client"}),
    awful.key({ dat.modkey, "Control" }, "n",
       function ()
          local c = awful.client.restore()
